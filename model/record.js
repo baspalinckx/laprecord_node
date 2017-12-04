330548d0a9f5d5c145841f64798da5d26e730709
baspalinckx <b.palinckx@student.avans.nl>
2017-12-04 19:05:43 +0100

    dummy data

--- model/record.js
+++ model/record.js
@@ -19,5 +19,35 @@ const RecordSchema = new Schema({
 
 const Record = mongoose.model('records', RecordSchema);
 
+Record.count({}, function (err, count) {
+    if(count < 5){
+        console.log('voeg recipe toe');
+        const Record = new Record({
+            time: '2:05:02',
+            weather: 'Sunny',
+            circuit:
+                {
+                    name: 'Circuit Zandvoort',
+                    country: 'The Netherlands',
+                    length: '4,3 KM'
+                },
+            car:
+                {
+                    brand: 'Mazda',
+                    model: 'MX-5',
+                    type: 'Roadster',
+                    year: 1992,
+                    modification: 'Coilovers, Upgraded brakes',
+                    tire: 'Advan Neova AD08R'
+                }
+
+
+        }).save();
+    }
+    else {
+        console.log('zit al een circuit in db')
+    }
+});
+
 
 module.exports = Record;
